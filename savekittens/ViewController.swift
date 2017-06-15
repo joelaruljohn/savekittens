f8e75ec75f3db27535f47213b18fb723e24e4113
Joel A. John <joel@Joels-MacBook-Pro-2.local>
2017-06-15 11:39:04 +0800

    Initial Commit

---
+++ savekittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savekittens
+//
+//  Created by Joel A. John on 15/6/17.
+//  Copyright © 2017 cortes.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
